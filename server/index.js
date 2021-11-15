0913011c9281f45cd3990dd09acfb477ffd3cb63
willtryagain <amanatman@gmail.com>
2021-11-16 01:46:16 +0530

    ignore node_modules

--- server/index.js
+++ server/index.js
@@ -26,3 +26,14 @@ app.use("/api/experiment", experiment);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('frontend/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
